ec3f2e9bfc9c0fe3b9a83968d307dccb0c452b09
Enxhi Sheta <enxhisheta@gmail.com>
2024-10-10 23:29:39 +0200

    added hero section

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <section id="about">
+      <div className="container">
+        <h2>Who am I?</h2>
+        <div className="about-text">
+          <p>
+            A passionate web developer with experience in creating dynamic and
+            responsive websites...
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { About };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section id="home" className="hero">
+      <h1>Welcome to NG Dev Space!</h1>
+      <h2>Your One-Stop Solution for All Your Web Needs</h2>
+      <p>I am Enxhi Sheta</p>
+      <p>Web Developer & Designer</p>
+      <a href="#projects" className="btn">
+        View My Work
+      </a>
+    </section>
+  );
+};
+
+export { Hero };
